0a67161dd8bb6407b121114191dc2acabb2c822f
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-12 13:50:38 -0300

    feat(companies): criacao do app voltado para empresas #8

---
+++ companies/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Enterprise(models.Model):
+    name = models.CharField(max_length=175)
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
