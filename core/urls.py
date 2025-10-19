19e0e9ee2c88eac384f678bd4f11f90ba459f7e8
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-19 13:12:40 -0300

    feat(backend): configurado as urls do site para incluir companies e configurado a vesão do app em parte da url #5

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')), # inclusao as rotas da app accounts
+    path('api/v1/auth/', include('accounts.urls')), # inclusao as rotas da app accounts
+    path('api/v1/companies/', include('companies.urls')), # inclusao as rotas da app companies
 ]
