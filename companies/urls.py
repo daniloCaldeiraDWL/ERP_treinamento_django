fb4dfeff19f76aee2d2bb15534a14e316f1f2b5c
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-19 15:55:55 -0300

    feat(backlog): incluir rotas de grupos e group detail nas urls, aula 29, issue #5

--- companies/urls.py
+++ companies/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from companies.views.employees import Employees, EmployeeDetail
 from companies.views.permissions import PermissionDetail
+from companies.views.groups import Groups, GroupDetail
 
 urlpatterns = [
     # employees endpoints
-    path('employees', Employees.as_view()),
-    path('employees/<int:employee_id>', EmployeeDetail.as_view()),
+    path('employees', Employees.as_view()), # rota para listar e criar funcionários
+    path('employees/<int:employee_id>', EmployeeDetail.as_view()), # rota para detalhes do funcionário específico, passando o id do funcionário
 
     # Groups And Permissions Endpoints
-    path('permissions', PermissionDetail.as_view()),
-
-] 
+    path('groups', Groups.as_view()), # rota para listar e criar grupos
+    path('groups/<int:group_id>', GroupDetail.as_view()), # rota para detalhes do grupo específico, passando o id do grupo
+    path('permissions', PermissionDetail.as_view()), # rota para detalhes das permissões dos grupos
+]
