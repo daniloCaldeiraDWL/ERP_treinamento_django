5e134ebf309f5f0952717797d32746c35356fb20
Danilo Caldeira <danilomartinscaldeira@gmail.com>
2025-10-19 14:27:51 -0300

    feat(backend): criado da view de permissions e configuração da url em companies, aula 27, issue #5

--- companies/urls.py
+++ companies/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from companies.views.employees import Employees, EmployeeDetail
+from companies.views.permissions import PermissionDetail
 
 urlpatterns = [
     # employees endpoints
     path('employees', Employees.as_view()),
-    path('employees/<int:employee_id>', EmployeeDetail.as_view())
+    path('employees/<int:employee_id>', EmployeeDetail.as_view()),
+
+    # Groups And Permissions Endpoints
+    path('permissions', PermissionDetail.as_view()),
+
 ] 
